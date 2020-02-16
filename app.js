2a87288c9e12398ee3c9b33e7fd75f1f862ced1b
Amit Kulkarni <amitsk_00@yahoo.com>
2020-02-16 15:06:02 +0530

    changes of 16Feb2020 #1

--- app.js
+++ app.js
@@ -1,12 +1,50 @@
-const fs = require("fs");
 const val = require("validator");
 const mydata = require("./mydata");
 const chalk = require("chalk");
-
-const msg = "Welcome to Node programing!!";
-console.log(chalk.red(msg));
-
-fs.writeFileSync("./tmp.txt", "data something ");
+const yargs = require("yargs");
 
 const myMsg = mydata();
-console.log(myMsg);
+// console.log(myMsg);
+
+const msg = "Welcome to Node programing!!";
+console.log(chalk.cyanBright(msg));
+
+// handle add paraemter
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function() {
+    console.log("Inside add function for notes");
+  }
+});
+
+// handle remove parameter
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function() {
+    console.log("Inside remove function for notes");
+  }
+});
+
+//handle list parameter
+yargs.command({
+  command: "list",
+  describe: "Describe a note",
+  handler: function() {
+    console.log("Inside list function for notes");
+  }
+});
+
+//handle help parameter
+yargs.command({
+  command: "help",
+  describe: "Help on the app",
+  handler: function() {
+    console.log("Inside help function for notes");
+  }
+});
+
+console.log(yargs.argv);
+
+console.log(chalk.bgRed.bold("End of the program ...."));
